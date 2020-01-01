587b3d2b0c4edf5e141d534f3727957c8c72704b
Roman Savinuk <savaroma@gmail.com>
2020-01-01 15:15:32 +0200

    added products

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()  // its working
+const app = express();  // its working
+const products = ['Apple', 'Pen', 'Computer'];
+
 
 app.get('/', (req, res, next) => {
     res.send('Its working!')
-})
+});
+
+app.get('/products', (req, res, next)=>{
+    console.log('Page', req.query.page);
+    res.json({products})
+});
+
+app.get('/products/:id', (req, res, next) =>{
+    if (products[req.params.id]){
+        res.send(products[req.params.id])
+    } else {
+        res.status(404).send('Product not found')
+    }
+});
 
 app.listen(5000, ()=>{
     console.log('Its started', new Date())
-})
+});
